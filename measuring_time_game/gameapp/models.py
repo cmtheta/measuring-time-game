ec4bf79da42271b64d8c8d86342b0e9e2369b501
shinshi <neneloophost@gmail.com>
2022-04-03 17:37:16 +0900

    feat made Record table in database

--- measuring_time_game/gameapp/models.py
+++ measuring_time_game/gameapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Record(models.Model):
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                    verbose_name="投稿者",
+                                    on_delete=models.CASCADE)
+    record = models.FloatField("記録", blank=False)
+    target_time = models.FloatField("目標時間", blank=False)
+    created_at = models.DateTimeField("記録日", auto_now=True)
+
+    def __str__(self):
+        return f'{self.created_by} {str(self.created_at)}'
