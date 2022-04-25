af43d2ab0a6baea7030bcdede2fdf627a808a4c5
shinshi <neneloophost@gmail.com>
2022-04-25 14:28:46 +0900

    fix setting 起動に必要な情報を消していたため、その情報を追加することで修正した

--- measuring_time_game/measuring_time_game/settings.py
+++ measuring_time_game/measuring_time_game/settings.py
@@ -19,6 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-zhtqoj&e&vv3otb5qt7wnc$c7e#m9bo*25d7&t@(@a3@ud_*7n'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
 # Application definition
 
 INSTALLED_APPS = [
